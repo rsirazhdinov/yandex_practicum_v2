9578213bf59ca626128a451df54a77c654c3e7c7
Sirazhdinov Ruslan <RSirazhdinov@beeline.ru>
2025-11-10 15:07:11 +0300

    fix

--- src/pages/modal-ingredient/modal-ingredient.tsx
+++ src/pages/modal-ingredient/modal-ingredient.tsx
@@ -1,13 +1,9 @@
-import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import React from 'react';
+import Modal from '@/components/modal/modal';
 import { useNavigate } from 'react-router-dom';
 
 import IngredientDetails from '@components/ingredient-details/ingredient-details.jsx';
-import ModalOverlay from '@components/modal-overlay/modal-overlay.jsx';
 
-import type { KeyboardEvent } from 'react';
-
-import modalStyles from '@components/modal/modal.module.css';
+import type React from 'react';
 
 export const ModalIngredient = (): React.JSX.Element => {
   const header = 'Детали ингредиента';
@@ -16,33 +12,10 @@ export const ModalIngredient = (): React.JSX.Element => {
   const onClose = (): void => {
     navigate(-1);
   };
-  React.useEffect(() => {
-    const handleEsc = (e: KeyboardEvent): void => {
-      if (e.key === 'Escape') {
-        onClose();
-      }
-    };
-    document.addEventListener('keydown', handleEsc);
-    return (): void => {
-      document.removeEventListener('keydown', handleEsc);
-    };
-  }, [onClose]);
 
   return (
-    <ModalOverlay onClick={onClose}>
-      <div className={` ${modalStyles.modal_popup}`}>
-        <div className={` pl-10 pt-10 pr-10 ${modalStyles.modal_header}`}>
-          <p className="text text_type_main-large">{header}</p>
-          <CloseIcon
-            className={modalStyles.close_icon}
-            type="primary"
-            onClick={onClose}
-          />
-        </div>
-        <div>
-          <IngredientDetails />
-        </div>
-      </div>
-    </ModalOverlay>
+    <Modal header={header} onClose={onClose}>
+      <IngredientDetails />
+    </Modal>
   );
 };
